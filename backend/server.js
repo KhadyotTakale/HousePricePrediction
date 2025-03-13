c7010dcf0f68b3aec819da7afbed8f96c580c6d7
KhadyotTakale <khadyottakale@gmail.com>
2025-03-13 18:55:45 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4005;
